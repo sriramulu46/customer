dc3212fabde6849f0d9b958a5320617d3485cd2f
Rajat-Jamblekar <Rajat-Jamblekar@users.noreply.github.com>
2024-03-13 17:40:01 +0530

    React js created

---
+++ src/service/CustomerService.js
@@ -0,0 +1,31 @@
+import axios from 'axios';
+
+const Customer_Base_URL = "http://localhost:8080/";
+
+class CustomerService {
+
+    // get all emp list
+    getAllEmp(){
+        return axios.get(Customer_Base_URL);
+    }
+
+    // save emp
+    saveEmp(emp){
+        return axios.post(Customer_Base_URL, emp);
+    }
+
+    // get all count list
+    getAllCount(){
+        return axios.get(Customer_Base_URL);
+    }
+
+    // save emp
+    saveCount(){
+        return axios.post(Customer_Base_URL);
+    }
+
+   
+}
+
+const CustomerService = new CustomerService();
+export default CustomerService;
